d012624d798a7f109f2e5a08cd792ba7949539f2
Vlad <tonkonoh.vlad@gmail.com>
2024-08-09 03:26:46 +0300

    create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from './components/Header';
+
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
 
 export default App;
